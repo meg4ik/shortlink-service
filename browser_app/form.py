5cbc981461c7ad4db9336dba62bc3d9a1cbd4597
meg4ik <59928647+meg4ik@users.noreply.github.com>
2022-10-29 23:53:56 +0300

    create view post

--- browser_app/form.py
+++ browser_app/form.py
@@ -6,3 +6,4 @@ class LinkForm(forms.ModelForm):
     class Meta:
         model = Link
         fields = ['full_link']
+        labels = {'full_link':'Your link:'}
--- browser_app/views.py
+++ browser_app/views.py
@@ -4,6 +4,8 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.views.generic.edit import CreateView
 from django.contrib.auth.forms import UserCreationForm
 from .form import LinkForm
+from django.core.exceptions import ValidationError
+from .models import Link, User
 
 # Create your views here.
 
@@ -13,6 +15,19 @@ class MainView(View):
             'form' : LinkForm
         }
         return render(request, 'main.html',context_data)
+
+    def post(self, request):
+        filled_form = LinkForm(request.POST)
+        if not filled_form.is_valid():
+            raise ValidationError()
+        if self.request.user.is_authenticated:
+            pass
+        link = Link(
+            full_link = request.POST.full_link,
+
+            )
+
+        return redirect('main')
 
 class ListView(View):
     def get(self, request):
